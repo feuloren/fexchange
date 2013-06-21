75f87431cf66129b55599440601f80d597b446db
Florent Thévenet <florent@fthevenet.fr>
2013-06-22 01:03:56 +0200

    Handler basique pour la recherche

---
+++ handlers/recherche.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+
+from .base import BaseHandler
+from models import Offre, Utilisateur
+
+class RechercheHandler(BaseHandler):
+    def get(self, term):
+        if term is None:
+            results = None
+        else:
+            results = self.db.query(Offre).filter(Offre.nom.like('%'+term+'%')).limit(20)
+        self.render("recherche.html", term = term,
+                    results = results)
