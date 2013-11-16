8d434614932df96145e324f749bb6b0ffd5673c9
Florent Thévenet <florent@fthevenet.fr>
2013-11-16 20:23:55 +0100

    Recherche : filtre sur le type d'article

--- app/handlers/recherche.py
+++ app/handlers/recherche.py
@@ -7,12 +7,20 @@ class RechercheHandler(BaseHandler):
     titre_ = 'Rechercher un meuble'
     def get(self):
         term = self.get_argument('q', None)
-        ventes = self.get_argument('accept-ventes', True)
-        dons = self.get_argument('accept-dons', True)
-        prets = self.get_argument('accept-prets', True)
+        ventes = self.get_argument('ventes', False)
+        dons = self.get_argument('dons', False)
+        prets = self.get_argument('prets', False)
         if term is None:
             results = None
         else:
-            results = self.db.query(Offre).filter(Offre.nom.like('%'+term+'%')).limit(20)
+            q = self.db.query(Offre).filter(Offre.nom.like('%'+term+'%'))
+            if not(ventes):
+                q = q.filter(Offre.type != 'vente')
+            if not(dons):
+                q = q.filter(Offre.type != 'don')
+            if not(prets):
+                q = q.filter(Offre.type != 'pret')
+
+            results = q.limit(20)
         self.render("recherche.html", term = term,
                     results = results)
