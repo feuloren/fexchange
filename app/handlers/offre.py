6c226c51785e81720da076976538d24c399c4e6c
Florent Thévenet <florent@fthevenet.fr>
2013-11-16 20:13:03 +0100

    Renommage de vente en offre dans plusieurs fichiers

--- app/handlers/offre.py
+++ app/handlers/offre.py
@@ -6,21 +6,21 @@ from ..models import *
 from .. import forms
 
 class ShowOffreHandler(BaseHandler):
-    titre_ = 'Vente'
+    titre_ = 'Offre'
 
     def get_categories(self):
         return [(c.id, c.nom) for c in self.db.query(Categorie).all()]
 
     def get(self, id):
-        vente = self.db.query(Offre).get(int(id))
-        if vente is None:
+        offre = self.db.query(Offre).get(int(id))
+        if offre is None:
             self.render("obj_not_found.html")
         else:
             if self.current_user:
-                if self.current_user is vente.vendeur:
-                    self.render("vente_manage.html", vente=vente)
+                if self.current_user is offre.vendeur:
+                    self.render("offre_manage.html", offre=offre)
                 else:
-                    # messages = vente.message.filter((de = self.user and pour = vente.vendeur) or (de = vente.vendeur and to=self.user)).order_by(date_envoi)
-                    self.rendeur("vente_show.html", vente=vente, messages=vente.message)
+                    # messages = offre.message.filter((de = self.user and pour = offre.vendeur) or (de = offre.vendeur and to=self.user)).order_by(date_envoi)
+                    self.rendeur("offre_show.html", offre=offre, messages=offre.message)
             else:
-                self.render("vente_show.html", vente=vente)
+                self.render("offre_show.html", offre=offre)
